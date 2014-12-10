b6d9139f81524e222bb8b29c56cc2217fe9b3db7
ctec student <ctecstudent@740s-204-pg-04.local>
2014-12-10 08:05:24 -0700

    Initial Commit

---
+++ StocktonIsStudlyTests/StocktonIsStudlyTests.swift
@@ -0,0 +1,36 @@
+//
+//  StocktonIsStudlyTests.swift
+//  StocktonIsStudlyTests
+//
+//  Created by ctec student on 12/10/14.
+//  Copyright (c) 2014 ctec student. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class StocktonIsStudlyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
